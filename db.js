2b417c49a616d0809d449a0517955e38fa870e1c
Abhishek Ranjan <happy@Abhisheks-MacBook-Air.local>
2025-04-04 00:11:52 +0530

    shifted mongodb to online server

--- db.js
+++ db.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
 
 // Define the mongoDB connection URL
-const mongoURL = 'mongodb://localhost:27017/mydatabase' // can replace 'mydatabase' with your database name
+//const mongoURL = process.env.MONGODB_URL_LOCAL     //'mongodb://localhost:27017/mydatabase' //  ye local url hai, can replace 'mydatabase' with your database name
+const mongoURL = process.env.MONGODB_URL
 
 //set up MongoDB connection
 mongoose.connect(mongoURL,{})
